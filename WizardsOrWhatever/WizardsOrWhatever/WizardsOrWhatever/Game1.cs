330483b8a62a28658c41b34a814f679789a32f4d
Esperanza De Castro <decastroesperanza@gmail.com>
2012-10-10 10:49:33 -0700

    Added Menus
    Menus are fully functioning and brad eats poop

--- WizardsOrWhatever/WizardsOrWhatever/WizardsOrWhatever/Game1.cs
+++ WizardsOrWhatever/WizardsOrWhatever/WizardsOrWhatever/Game1.cs
@@ -1,6 +1,14 @@
-using System;
+#region File Description
+//-----------------------------------------------------------------------------
+// Game.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
 using System.Collections.Generic;
-//using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -15,230 +23,115 @@ using FarseerPhysics.Common;
 using FarseerPhysics.Collision;
 using FarseerPhysics.DebugViews;
 using FarseerPhysics;
+#endregion
 
 namespace WizardsOrWhatever
 {
     /// <summary>
-    /// This is the main type for your game
+    /// Sample showing how to manage different game states, with transitions
+    /// between menu screens, a loading screen, the game itself, and a pause
+    /// menu. This main game class is extremely simple: all the interesting
+    /// stuff happens in the ScreenManager component.
     /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class GameStateManagementGame : Microsoft.Xna.Framework.Game
     {
-        //Adds a new camera object, follows the character
-        Camera camera;
+        #region Fields
 
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        ScreenManager screenManager;
+        //int viewportHeight;
+        //int viewportWidth;
 
-        World world;
+        // By preloading any assets used by UI rendering, we avoid framerate glitches
+        // when they suddenly need to be loaded in the middle of a menu transition.
+        static readonly string[] preloadAssets =
+        {
+            "gradient",
+        };
 
-        //Position of character to be followed by the camera
-        CompositeCharacter player;
 
-        MSTerrain terrain;
+        #endregion
 
-        PhysicsObject ground;
-        PhysicsObject leftWall;
-        PhysicsObject rightWall;
-        PhysicsObject ceiling;
+        #region Initialization
 
-        List<PhysicsObject> paddles;
 
-        //TODO: get DebugView working properly
-        //TODO: get MSTerrain working properly
-        DebugViewXNA DebugView;
-
-        private Matrix projection, view;
-
-        public Game1()
+        /// <summary>
+        /// The main game constructor.
+        /// </summary>
+        public GameStateManagementGame()
         {
             Content.RootDirectory = "Content";
+
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 853;
+            graphics.PreferredBackBufferHeight = 480;
 
-            world = new World(new Vector2(0, 9.8f));
+            //viewportHeight = GraphicsDevice.Viewport.Height;
+            //viewportWidth = GraphicsDevice.Viewport.Width;
 
-            terrain = new MSTerrain(world, new AABB(new Vector2(0, 0), 80, 80))
-            {
-                PointsPerUnit = 10,
-                CellSize = 50,
-                SubCellSize = 5,
-                Decomposer = Decomposer.Earclip,
-                Iterations = 2
-            };
 
-            terrain.Initialize();
+            // Create the screen manager component.
+            screenManager = new ScreenManager(this);
+
+            Components.Add(screenManager);
+
+            // Activate the first screens.
+            screenManager.AddScreen(new BackgroundScreen(), null);
+            screenManager.AddScreen(new MainMenuScreen(), null);
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-            camera = new Camera(GraphicsDevice.Viewport);
-            Settings.EnableDiagnostics = true;
-            DebugView = new DebugViewXNA(world);
-            DebugView.LoadContent(GraphicsDevice, Content);
-            projection = Matrix.CreateOrthographic(
-                graphics.PreferredBackBufferWidth / 100.0f,
-                -graphics.PreferredBackBufferHeight / 100.0f, 0, 1000000);
-            Vector3 campos = new Vector3();
-            campos.X = (-graphics.PreferredBackBufferWidth / 2) / 100.0f;
-            campos.Y = (graphics.PreferredBackBufferHeight / 2) / -100.0f;
-            campos.Z = 0;
-            Matrix tran = Matrix.Identity;
-            tran.Translation = campos;
-            view = tran;
-            base.Initialize();
-        }
 
         /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
+        /// Loads graphics content.
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            //terrain.ApplyTexture(Content.Load<Texture2D>("Terrain"), new Vector2(200, 0), InsideTerrainTest);
-
-            player = new CompositeCharacter(world, new Vector2(GraphicsDevice.Viewport.Width / 2.0f, GraphicsDevice.Viewport.Height / 2.0f),
-                Content.Load<Texture2D>("bean_ss1"), new Vector2(35.0f, 50.0f));
-
-            ground = new StaticPhysicsObject(world, new Vector2(GraphicsDevice.Viewport.Width / 2.0f, GraphicsDevice.Viewport.Height - 12.5f),
-                Content.Load<Texture2D>("platformTex"), new Vector2(GraphicsDevice.Viewport.Width, 25.0f));
-
-            //leftWall = new StaticPhysicsObject(world, new Vector2(12.5f, GraphicsDevice.Viewport.Height / 2.0f),
-                //Content.Load<Texture2D>("platformTex"), new Vector2(25.0f, GraphicsDevice.Viewport.Height));
-
-            //rightWall = new StaticPhysicsObject(world, new Vector2(GraphicsDevice.Viewport.Width - 12.5f, GraphicsDevice.Viewport.Height / 2.0f),
-                //Content.Load<Texture2D>("platformTex"), new Vector2(25.0f, GraphicsDevice.Viewport.Height));
-
-            ceiling = new StaticPhysicsObject(world, new Vector2(GraphicsDevice.Viewport.Width / 2.0f, 12.5f),
-                Content.Load<Texture2D>("platformTex"), new Vector2(GraphicsDevice.Viewport.Width, 25.0f));
-
-            // *** Stolen from demo ***
-
-            paddles = new List<PhysicsObject>();
-
-            // Creates a simple paddle which center is anchored
-            // in the background. It can rotate freely
-            PhysicsObject simplePaddle = new PhysicsObject(world, new Vector2(),
-                Content.Load<Texture2D>("Paddle"), new Vector2(128, 16), 10);
-
-            JointFactory.CreateFixedRevoluteJoint(world, simplePaddle.body, ConvertUnits.ToSimUnits(new Vector2(0, 0)),
-                ConvertUnits.ToSimUnits(new Vector2(GraphicsDevice.Viewport.Width / 2.0f - 150, GraphicsDevice.Viewport.Height - 300)));
-
-            paddles.Add(simplePaddle);
-
-            // Creates a motorized paddle which left side is anchored in the background
-            // it will rotate slowly but the motoro is not set too strong that
-            // it can push everything away
-            PhysicsObject motorPaddle = new PhysicsObject(world, new Vector2(GraphicsDevice.Viewport.Width / 2.0f, GraphicsDevice.Viewport.Height - 280),
-                Content.Load<Texture2D>("Paddle"), new Vector2(128, 16), 10);
-
-            var j = JointFactory.CreateFixedRevoluteJoint(world, motorPaddle.body, ConvertUnits.ToSimUnits(new Vector2(-48, 0)),
-                ConvertUnits.ToSimUnits(new Vector2(GraphicsDevice.Viewport.Width / 2.0f, GraphicsDevice.Viewport.Height - 280)));
-
-            // rotate 1/4 of a circle per second
-            j.MotorSpeed = MathHelper.PiOver2;
-            // have little torque (power) so it can push away a few blocks
-            j.MotorTorque = 50;
-            j.MotorEnabled = true;
-            j.MaxMotorTorque = 100;
-
-            paddles.Add(motorPaddle);
-
-            // Use two line joints (a sort of springs) to create a trampoline
-            PhysicsObject trampolinePaddle = new PhysicsObject(world, new Vector2(600, ground.Position.Y - 175), Content.Load<Texture2D>("Paddle"), new Vector2(128, 16), 10);
-
-            var l = JointFactory.CreateLineJoint(ground.body, trampolinePaddle.body, ConvertUnits.ToSimUnits(trampolinePaddle.Position - new Vector2(64, 0)), Vector2.UnitY);
-
-            l.CollideConnected = true;
-            l.Frequency = 2.0f;
-            l.DampingRatio = 0.05f;
-
-            var r = JointFactory.CreateLineJoint(ground.body, trampolinePaddle.body, ConvertUnits.ToSimUnits(trampolinePaddle.Position + new Vector2(64, 0)), Vector2.UnitY);
-
-            r.CollideConnected = true;
-            r.Frequency = 2.0f;
-            r.DampingRatio = 0.05f;
-
-            world.AddJoint(l);
-            world.AddJoint(r);
-
-            paddles.Add(trampolinePaddle);
-        }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            GamePadState currentState = GamePad.GetState(PlayerIndex.One);
-            if (currentState.IsConnected)
+            foreach (string asset in preloadAssets)
             {
-                player.move(currentState);
+                Content.Load<object>(asset);
             }
-            else
-            {
-                player.move();
-            } 
 
-            player.Update(gameTime);
-            
-            world.Step((float)gameTime.ElapsedGameTime.TotalSeconds);
 
-            base.Update(gameTime);
+
         }
+
+
+        #endregion
+
+        #region Draw
+
 
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            graphics.GraphicsDevice.Clear(Color.Black);
 
-            //DebugView.RenderDebugData(ref projection, ref view);
-
-            //camer.transform applies the matrix transform to the sprite base to move with the camera
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, camera.transform);
-
-            ground.Draw(spriteBatch);
-            //leftWall.Draw(spriteBatch);
-            //rightWall.Draw(spriteBatch);
-            ceiling.Draw(spriteBatch);
-
-            player.Draw(spriteBatch);
-
-            foreach (PhysicsObject paddle in paddles)
-            {
-                paddle.Draw(spriteBatch);
-            }
-
-            spriteBatch.End();
-            camera.Update(player);
+            // The real drawing happens inside the screen manager component.
             base.Draw(gameTime);
         }
 
-        private bool InsideTerrainTest(Color color)
+
+        #endregion
+    }
+
+
+    #region Entry Point
+
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    static class Program
+    {
+        static void Main()
         {
-            return color == Color.Black;
+            using (GameStateManagementGame game = new GameStateManagementGame())
+            {
+                game.Run();
+            }
         }
     }
+
+    #endregion
 }
