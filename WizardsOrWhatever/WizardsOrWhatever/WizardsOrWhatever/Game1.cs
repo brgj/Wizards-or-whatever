04f960b65cd0b265daf84fa25c22caeff1aed4b2
brgj <brad.johnson155@gmail.com>
2012-10-10 03:43:55 +0100

    Added comments to Game1.cs, working on implementing terrain and debug mode, ignore commented out code

--- WizardsOrWhatever/WizardsOrWhatever/WizardsOrWhatever/Game1.cs
+++ WizardsOrWhatever/WizardsOrWhatever/WizardsOrWhatever/Game1.cs
@@ -23,49 +23,45 @@ namespace WizardsOrWhatever
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
-        //Adds a new camera object, follows the character
-        Camera camera;
-
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-
+        //The world object that encapsulates all physics objects
         World world;
-
-        //Position of character to be followed by the camera
-        CompositeCharacter player;
-
         MSTerrain terrain;
 
+        //Variable for accessing the graphics device manager of the current game
+        GraphicsDeviceManager graphics;
+        //The spritebatch used for drawing the current game
+        SpriteBatch spriteBatch;
+
+        //Adds a new camera object, follows the character
+        Camera camera;
+        //Game character controlled by user. TODO: Use a list for offline multiplayer
+        CompositeCharacter player;
+
+        //Walls. Placeholders for terrain.
         PhysicsObject ground;
         PhysicsObject leftWall;
         PhysicsObject rightWall;
         PhysicsObject ceiling;
 
+        //List of paddles with different properties to be drawn to screen. Placeholder for actual interesting content.
         List<PhysicsObject> paddles;
+
+        //Debug View. For viewing all underlying physics components of game.
+        DebugViewXNA DebugView;
+
+        //For Debug View. Temporary until fixed.
+        private Matrix projection, view;
+
 
         //TODO: get DebugView working properly
         //TODO: get MSTerrain working properly
-        DebugViewXNA DebugView;
-
-        private Matrix projection, view;
-
+        //The y pos is losing accuracy at some point during run time.
+        //Debug more and figure out the location
+        //Figure out debug mode and implement new terrain.
         public Game1()
         {
             Content.RootDirectory = "Content";
             graphics = new GraphicsDeviceManager(this);
-
-            world = new World(new Vector2(0, 9.8f));
-
-            terrain = new MSTerrain(world, new AABB(new Vector2(0, 0), 80, 80))
-            {
-                PointsPerUnit = 10,
-                CellSize = 50,
-                SubCellSize = 5,
-                Decomposer = Decomposer.Earclip,
-                Iterations = 2
-            };
-
-            terrain.Initialize();
         }
 
         /// <summary>
@@ -76,8 +72,24 @@ namespace WizardsOrWhatever
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            camera = new Camera(GraphicsDevice.Viewport);
+            // Create new SpriteBatch from GraphicsDevice
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //Create world with gravity = 9.8 m/s downwards
+            world = new World(new Vector2(0, 9.8f));
+
+            //Create and initialize terrain
+            terrain = new MSTerrain(world, new AABB(new Vector2(0, 0), 80, 80))
+            {
+                PointsPerUnit = 10,
+                CellSize = 50,
+                SubCellSize = 5,
+                Decomposer = Decomposer.Earclip,
+                Iterations = 2
+            };
+            terrain.Initialize();
+
+            // DebugView stuff
             Settings.EnableDiagnostics = true;
             DebugView = new DebugViewXNA(world);
             DebugView.LoadContent(GraphicsDevice, Content);
@@ -91,6 +103,7 @@ namespace WizardsOrWhatever
             Matrix tran = Matrix.Identity;
             tran.Translation = campos;
             view = tran;
+
             base.Initialize();
         }
 
@@ -100,28 +113,26 @@ namespace WizardsOrWhatever
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Create camera using current viewport
+            camera = new Camera(GraphicsDevice.Viewport);
 
             //terrain.ApplyTexture(Content.Load<Texture2D>("Terrain"), new Vector2(200, 0), InsideTerrainTest);
 
+            //Create player
             player = new CompositeCharacter(world, new Vector2(GraphicsDevice.Viewport.Width / 2.0f, GraphicsDevice.Viewport.Height / 2.0f),
                 Content.Load<Texture2D>("bean_ss1"), new Vector2(35.0f, 50.0f));
 
+            //Create walls
             ground = new StaticPhysicsObject(world, new Vector2(GraphicsDevice.Viewport.Width / 2.0f, GraphicsDevice.Viewport.Height - 12.5f),
                 Content.Load<Texture2D>("platformTex"), new Vector2(GraphicsDevice.Viewport.Width, 25.0f));
-
             //leftWall = new StaticPhysicsObject(world, new Vector2(12.5f, GraphicsDevice.Viewport.Height / 2.0f),
                 //Content.Load<Texture2D>("platformTex"), new Vector2(25.0f, GraphicsDevice.Viewport.Height));
-
             //rightWall = new StaticPhysicsObject(world, new Vector2(GraphicsDevice.Viewport.Width - 12.5f, GraphicsDevice.Viewport.Height / 2.0f),
                 //Content.Load<Texture2D>("platformTex"), new Vector2(25.0f, GraphicsDevice.Viewport.Height));
-
             ceiling = new StaticPhysicsObject(world, new Vector2(GraphicsDevice.Viewport.Width / 2.0f, 12.5f),
                 Content.Load<Texture2D>("platformTex"), new Vector2(GraphicsDevice.Viewport.Width, 25.0f));
 
-            // *** Stolen from demo ***
-
+            //Instantiate a list of paddles to be used
             paddles = new List<PhysicsObject>();
 
             // Creates a simple paddle which center is anchored
@@ -189,7 +200,9 @@ namespace WizardsOrWhatever
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            //Stores state of an xbox controller
             GamePadState currentState = GamePad.GetState(PlayerIndex.One);
+
             if (currentState.IsConnected)
             {
                 player.move(currentState);
@@ -200,9 +213,13 @@ namespace WizardsOrWhatever
             } 
 
             player.Update(gameTime);
-            
+            camera.Update(player);
+
+            //Notifies the world that time has progressed.
+            //Collision detection, integration, and constraint solution are performed
             world.Step((float)gameTime.ElapsedGameTime.TotalSeconds);
 
+            //Update other components tracked by game
             base.Update(gameTime);
         }
 
@@ -212,11 +229,12 @@ namespace WizardsOrWhatever
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            //Background drawn to CornflowerBlue
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             //DebugView.RenderDebugData(ref projection, ref view);
 
-            //camer.transform applies the matrix transform to the sprite base to move with the camera
+            //Begins spriteBatch with the default sort mode, alpha blending on sprites, and a camera.
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, camera.transform);
 
             ground.Draw(spriteBatch);
@@ -232,13 +250,18 @@ namespace WizardsOrWhatever
             }
 
             spriteBatch.End();
-            camera.Update(player);
             base.Draw(gameTime);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
         private bool InsideTerrainTest(Color color)
         {
-            return color == Color.Black;
+            throw new NotImplementedException();
+            //return color == Color.Black;
         }
     }
 }
